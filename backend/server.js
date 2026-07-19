7554a14f9bcedb85e49bb25693508fa4c7a960ba
MalihaChowdhuryTasnim <chowdhurymalihatasnim@gmail.com>
2026-07-19 13:37:10 +0600

    Added userRutes

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/", userRoutes);
 
 const PORT = 3000;
 
